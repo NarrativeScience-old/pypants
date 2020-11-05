4ab91688e9f3b416ff17d93fc4ec4007d37ff8a6
Brian DeSimone <bdesimone@narrativescience.com>
2020-11-04 18:08:20 -0600

    behave uses the common lib resource builder

--- src/pypants/build_targets/behave_.py
+++ src/pypants/build_targets/behave_.py
@@ -1,6 +1,5 @@
 """Contains PythonBehaveTestPackage class"""
 import ast
-from typing import Optional
 
 from .test import PythonTestPackage
 
@@ -27,27 +26,13 @@ class PythonBehaveTestPackage(PythonTestPackage):
         )
         return node
 
-    def _generate_python_resources_behave_wrapper_node(
-        self, name: Optional[str] = "resources", resource_path: Optional[str] = "**/*"
-    ) -> ast.Expr:
-        """Generates an AST node for a python_resources target that wraps up resources and files as dependencies"""
-        keywords = [
-            ast.keyword(arg="sources", value=ast.List(elts=[ast.Str(resource_path)]))
-        ]
-        if name is not None:
-            keywords.insert(0, ast.keyword(arg="name", value=ast.Str(name)))
-        node = ast.Expr(
-            value=ast.Call(func=ast.Name(id="resources"), args=[], keywords=keywords)
-        )
-        return node
-
     def generate_build_file_ast_node(self) -> ast.Module:
         """Generate a Pants BUILD file as an AST module node"""
         node = ast.Module(
             body=[
                 self._generate_python_library_ast_node(name="lib"),
                 self._generate_python_binary_behave_wrapper_node(),
-                self._generate_python_resources_behave_wrapper_node(),
+                self._generate_python_library_resources_ast_node(),
             ]
         )
         return node
